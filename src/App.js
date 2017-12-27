fd29bf5939e562d601178f0ac68b805481134c5e
silentarrowz <silentarrowz@gmail.com>
2017-12-27 17:37:48 +0530

    added more files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MainApp from './MainApp';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <MainApp/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class Login extends Component{
+	render(){
+		return(
+			<div>
+			<h2>This is the login page</h2>
+
+			</div>
+
+			)
+	}
+};
